bff2c3282236ea40c76cf00736c57c7ea7ac9f1e
Kostiantyn Spyrydonov <kostiantynspyrydonov@gmail.com>
2025-12-15 13:08:15 +0100

    pre-build setup

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
+import Raty from 'raty-js';
