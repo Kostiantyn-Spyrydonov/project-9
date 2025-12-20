66c3286e68e75a72aa65d493a70a9b36128baf7f
Kostiantyn Spyrydonov <kostiantynspyrydonov@gmail.com>
2025-12-20 10:05:08 +0100

    fix FAQ functionality

--- src/js/FAQ.js
+++ src/js/FAQ.js
@@ -1,12 +1,18 @@
 import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
 export function initFaq() {
+  const faqContainer = document.querySelector('.faq');
+  
+  if (!faqContainer) return;
 
-    new Accordion('.faq',
-        {
-            elementClass: 'faq-item',
-            triggerClass: 'faq-question',
-            panelClass: 'faq-answer',
-            openMultiple: false
-        });
+  new Accordion('.faq', {
+    elementClass: 'faq-item',
+    triggerClass: 'faq-question',
+    panelClass: 'faq-answer',
+    activeClass: 'active',
+    openOnInit: [],
+    collapse: true,
+    showMultiple: false,
+  });
 }
